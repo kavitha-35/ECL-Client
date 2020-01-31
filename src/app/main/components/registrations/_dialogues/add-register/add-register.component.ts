4167f14e5cfc66fe1659cb203b6e96b0e4cba6d7
A J I N <ajin1099@gmail.com>
2020-01-31 19:45:21 +0530

    //

--- src/app/main/components/registrations/_dialogues/add-register/add-register.component.ts
+++ src/app/main/components/registrations/_dialogues/add-register/add-register.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { NgForm } from '@angular/forms';
+import { RegistrationModel } from 'app/main/models/registration/registration.model';
+import { IndividualTestModel } from 'app/control-panel/models/test-master/individual-test/individual-test.model';
+import { CombinedTestService } from 'app/control-panel/services/combinedtest.service';
 
 @Component({
   selector: 'app-add-register',
@@ -9,8 +12,12 @@ import { NgForm } from '@angular/forms';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AddRegisterComponent implements OnInit {
+  public tests: IndividualTestModel[];
+  selectedTests: IndividualTestModel[];
+  isBusy: boolean;
 
   constructor(
+    private readonly _combinedTestService: CombinedTestService,
     private readonly _dialogRef: MatDialogRef<AddRegisterComponent>,
     /*private readonly commentService: CommentService,*/
   ) {}
@@ -23,5 +30,19 @@ export class AddRegisterComponent implements OnInit {
     /*this.commentService.addComment(comment.form.value).subscribe((data) => {
       this._dialogRef.close();
     });*/
+  }
+  public searchQuery(event: any): void {
+    this.selectedTests = this._filter(event.target.value);
+  }
+  private _filter(value: string): IndividualTestModel[] {
+    const filterValue = value.toLowerCase();
+    return this.tests.filter((option) => option.testComponent.toLowerCase().includes(filterValue));
+  }
+  public getAllIndividualTest(): void {
+    this.isBusy = true;
+    this._combinedTestService.getAllIndividualTests().subscribe((data: IndividualTestModel[]) => {
+      this.tests = data;
+      this.isBusy = false;
+    });
   }
 }
