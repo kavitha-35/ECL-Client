0c4c0037cd0666d21cb3b2ede75319f699f58dd6
A J I N <ajin1099@gmail.com>
2020-02-01 09:53:02 +0530

    //

--- src/app/main/components/registrations/_dialogues/add-register/add-register.component.ts
+++ src/app/main/components/registrations/_dialogues/add-register/add-register.component.ts
@@ -1,26 +1,34 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ViewChild } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { NgForm } from '@angular/forms';
 import { RegistrationModel } from 'app/main/models/registration/registration.model';
 import { IndividualTestModel } from 'app/control-panel/models/test-master/individual-test/individual-test.model';
 import { CombinedTestService } from 'app/control-panel/services/combinedtest.service';
+import { MatTable } from '@angular/material';
 
 @Component({
   selector: 'app-add-register',
   templateUrl: './add-register.component.html',
   styleUrls: ['./add-register.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AddRegisterComponent implements OnInit {
   public tests: IndividualTestModel[];
   selectedTests: IndividualTestModel[];
   isBusy: boolean;
-
+  groupById: number;
+  orderById: number;
+  testsInTable: IndividualTestModel[] = [];
+  @ViewChild(MatTable, { static: false }) _matTable: MatTable<any>;
   constructor(
     private readonly _combinedTestService: CombinedTestService,
-    private readonly _dialogRef: MatDialogRef<AddRegisterComponent>,
-    /*private readonly commentService: CommentService,*/
-  ) {}
+    private readonly _dialogRef: MatDialogRef<
+      AddRegisterComponent
+    > /*private readonly commentService: CommentService,*/,
+  ) {
+    this.groupById = 0;
+    this.orderById = 0;
+  }
 
   ngOnInit(): void {}
 
@@ -30,6 +38,13 @@ export class AddRegisterComponent implements OnInit {
     /*this.commentService.addComment(comment.form.value).subscribe((data) => {
       this._dialogRef.close();
     });*/
+  }
+  public selectedAutoComplete(element: IndividualTestModel): void {
+    element.groupById = this.groupById;
+    element.orderById = this.orderById;
+    this.testsInTable.push(element);
+    console.log(this.testsInTable);
+    this._matTable.renderRows();
   }
   public searchQuery(event: any): void {
     this.selectedTests = this._filter(event.target.value);
