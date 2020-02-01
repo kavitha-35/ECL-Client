ce4fb1dc7452e8e03828afbd1124d0e8998e3f7a
Sathish <sathish.2009btech@gmail.com>
2020-02-01 07:56:27 +0530

    autosearch test information

--- src/app/main/components/registrations/_dialogues/add-register/add-register.component.ts
+++ src/app/main/components/registrations/_dialogues/add-register/add-register.component.ts
@@ -4,6 +4,7 @@ import { NgForm } from '@angular/forms';
 import { RegistrationModel } from 'app/main/models/registration/registration.model';
 import { IndividualTestModel } from 'app/control-panel/models/test-master/individual-test/individual-test.model';
 import { CombinedTestService } from 'app/control-panel/services/combinedtest.service';
+import { CombinedTestModel } from 'app/control-panel/models/test-master/combined-test/combined-test.model';
 
 @Component({
   selector: 'app-add-register',
@@ -12,8 +13,12 @@ import { CombinedTestService } from 'app/control-panel/services/combinedtest.ser
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AddRegisterComponent implements OnInit {
-  public tests: IndividualTestModel[];
-  selectedTests: IndividualTestModel[];
+  // public tests: IndividualTestModel[];
+  // selectedTests: IndividualTestModel[];
+
+  public combinedTestData          : CombinedTestModel[] = [];
+  public combinedTests: CombinedTestModel[];
+  public selectedCombinedTests: CombinedTestModel[];
   isBusy: boolean;
 
   constructor(
@@ -22,7 +27,9 @@ export class AddRegisterComponent implements OnInit {
     /*private readonly commentService: CommentService,*/
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getAllCombinedTest();
+  }
 
   public onAddRegisterClicked(registration: NgForm): void {
     /*comment.form.value.lookupName = 'comment';
@@ -31,17 +38,27 @@ export class AddRegisterComponent implements OnInit {
       this._dialogRef.close();
     });*/
   }
+
   public searchQuery(event: any): void {
-    this.selectedTests = this._filter(event.target.value);
+    this.selectedCombinedTests = this._filter(event.target.value);
   }
-  private _filter(value: string): IndividualTestModel[] {
+
+  private _filter(value: string): CombinedTestModel[] {
     const filterValue = value.toLowerCase();
-    return this.tests.filter((option) => option.testComponent.toLowerCase().includes(filterValue));
+    return this.combinedTests.filter((option) => option.testName.toLowerCase().includes(filterValue));
   }
-  public getAllIndividualTest(): void {
+
+  public selectedAutoComplete(element: CombinedTestModel): void {
+    this.combinedTestData.push(element);
+    console.log('combined test selected result', this.combinedTestData);
+    // this.linkTestComponent.refreshTable();
+  }
+
+  public getAllCombinedTest(): void {
     this.isBusy = true;
-    this._combinedTestService.getAllIndividualTests().subscribe((data: IndividualTestModel[]) => {
-      this.tests = data;
+    this._combinedTestService.getAllTests().subscribe((data: CombinedTestModel[]) => {
+      console.log('combined test result', data);
+      this.combinedTests = data;
       this.isBusy = false;
     });
   }
