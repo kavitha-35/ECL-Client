4377621b80c6e3980be30399c52ee90b9c61b4f3
Sathish <sathish.2009btech@gmail.com>
2020-02-01 10:58:55 +0530

    save petient regisrtaion API integration

--- src/app/main/components/registrations/_dialogues/add-register/add-register.component.ts
+++ src/app/main/components/registrations/_dialogues/add-register/add-register.component.ts
@@ -5,6 +5,7 @@ import { RegistrationModel } from 'app/main/models/registration/registration.mod
 import { IndividualTestModel } from 'app/control-panel/models/test-master/individual-test/individual-test.model';
 import { CombinedTestService } from 'app/control-panel/services/combinedtest.service';
 import { CombinedTestModel } from 'app/control-panel/models/test-master/combined-test/combined-test.model';
+import { PatientServices } from 'app/main/services/patient.services';
 
 @Component({
   selector: 'app-add-register',
@@ -25,18 +26,17 @@ export class AddRegisterComponent implements OnInit {
     private readonly _combinedTestService: CombinedTestService,
     private readonly _dialogRef: MatDialogRef<AddRegisterComponent>,
     /*private readonly commentService: CommentService,*/
+    private readonly _patientService: PatientServices
   ) {}
 
   ngOnInit(): void {
     this.getAllCombinedTest();
   }
 
-  public onAddRegisterClicked(registration: NgForm): void {
-    /*comment.form.value.lookupName = 'comment';
-    console.log(comment.form.value);
-    /*this.commentService.addComment(comment.form.value).subscribe((data) => {
+  public onAddRegisterClicked(): void {
+    this._patientService.savePatient(null).subscribe((data) => {
       this._dialogRef.close();
-    });*/
+    });
   }
 
   public searchQuery(event: any): void {
--- src/app/main/services/patient.services.ts
+++ src/app/main/services/patient.services.ts
@@ -6,10 +6,11 @@ import { ROUTE_CONFIG } from '../../shared/models/constant';
 import { AddPatientRequestModel } from '../models/patient/registration/add-patient-request.model';
 import { AddPatientResponseModel } from '../models/patient/registration/add-patient-response.model';
 import { GetPatientResponseModel } from '../models/patient/get-patient/get-patient-response.model';
+import { PatientModel } from '../models/patient/patient.model';
 
 @Injectable()
 export class PatientServices {
-  private readonly routePrefix = 'api/v1/Patients';
+  private readonly routePrefix = 'registration';
 
   constructor(private httpClient: HttpClient) {}
 
@@ -26,5 +27,10 @@ export class PatientServices {
   searchPatients(query: string): Observable<GetPatientResponseModel[]> {
     const apiUrl = `${ROUTE_CONFIG.PatientServiceBaseUrl}/${this.routePrefix}?searchQuery=${query}`;
     return this.httpClient.put<GetPatientResponseModel[]>(apiUrl, null);
+  }
+
+  savePatient(patient: PatientModel): Observable<PatientModel> {
+    const apiUrl = `${ROUTE_CONFIG.PatientServiceBaseUrl}/${this.routePrefix}`;
+    return this.httpClient.post<PatientModel>(apiUrl, patient);
   }
 }
