f5285dec597ae655bd959bb3570ea13e104f5815
AlbinLijo <albinlijo1997@gmail.com>
2020-02-02 17:54:23 +0530

    ui fixes

--- src/app/main/components/dashboards/lab-managment/lab-managment.component.ts
+++ src/app/main/components/dashboards/lab-managment/lab-managment.component.ts
@@ -10,7 +10,7 @@ export class LabManagmentComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
 }
--- src/app/main/components/dashboards/operations-dashboard/operations-dashboard.component.ts
+++ src/app/main/components/dashboards/operations-dashboard/operations-dashboard.component.ts
@@ -10,7 +10,7 @@ export class OperationsDashboardComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
 }
